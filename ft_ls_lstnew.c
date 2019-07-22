f0b0d53c405cf5dd053fe6628aceac1c765f297b
Zhain HELM <zhelm@c5r5s5.wethinkcode.co.za>
2019-07-22 15:22:29 +0200

    ok

--- ft_ls_lstnew.c
+++ ft_ls_lstnew.c
@@ -2,16 +2,17 @@
 
 t_ls *ft_ls_lstnew(struct stat *sb, struct dirent *de)
 {
-	t_ls *new;//need to figure out how to do this
+	t_ls *new;
 
 	if(!(new = (t_ls *)malloc(sizeof(t_ls))))
 		return (NULL);
-	if(!(new->sb = malloc(sizeof(sb))) || !(new->de = malloc(sizeof(de))))
-		{
-			free(new);
-			return NULL;
-		}
+	// if(!(new->sb = malloc(sizeof(sb))) || !(new->de = malloc(sizeof(de))))
+	// 	{
+	// 		free(new);
+	// 		return NULL;
+	// 	}
 	new->sb = sb;
 	new->de = de;
 	new->next = NULL;
+	return(new);
 }
--- main.c
+++ main.c
@@ -9,14 +9,62 @@
 /*   Updated: 2019/07/15 10:56:10 by zhelm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "ft_ls.h"
-#include "libft.h"
+#include "libft/libft.h"
 
+void ft_listsort(t_ls *head)
+{
+	t_ls *ptr;
+	t_ls *hold;
+	t_ls *test;
+	t_ls *tmp;
 
+	test = head;
+	while (test->next != NULL)
+	{
+		printf("%s\t", test->de->d_name);
+		test = test->next;
+	}
+	printf("\n");
+	ptr = head;
+	while (ptr->next != NULL)
+	{
+		if (ft_strcmp(ptr->de->d_name, ptr->next->de->d_name) > 0)
+		{
+			hold = ptr->next;
+			ptr->next = ptr->next->next;
+			//hold->next = NULL;
+			ptr = head;
+			while (ptr->next != NULL)
+			{
+				if (ft_strcmp(hold->de->d_name, ptr->de->d_name) > 0 && ft_strcmp(hold->de->d_name, ptr->next->de->d_name) < 0)
+				{
+					hold->next = ptr->next;
+					ptr->next = hold;
+					ptr = head;
+					break;
+				}else
+					ptr = ptr->next;
+			}
+			break;
+			//ptr = ptr->next;
+		}
+		else
+			ptr = ptr->next;
+		// else
+		//break;
+	}
+	while (head->next != NULL)
+	{
+		printf("%s\t", head->de->d_name);
+		head = head->next;
+	}
+}
 int main() //main to test the ctime and mtime
 {
 	t_ls *list;
- 	t_ls *ptr;
+	t_ls *ptr;
 	struct stat buf; //ctime is the last time the inode changed and mtime is the last time the file was modified.
 	char mtime[100];
 	struct dirent *de;
@@ -32,33 +80,33 @@ int main() //main to test the ctime and mtime
 	while ((de = readdir(dr)) != NULL)
 	{
 		stat(de->d_name, &sb);
-		if(list == NULL)
+		if (list == NULL)
 			list = ft_ls_lstnew(&sb, de);
 		else
-		{
 			ft_ls_lstadd(&list, ft_ls_lstnew(&sb, de));
-			list = list->next;//I think the list works now
-		}
-		if(list->de->d_type == 8)
-		{
-			printf("%s  ", list->de->d_name);
-			//printf("%ld\n", list->sb->st_nlink);//number of links
-		}
+
+		//printf("OK");printf("%s", list->de->d_name);
+		//if(list->de->d_type == 8)
+		//{
+		//printf("%s\t", list->de->d_name);
+		//printf("%o\n", sb.st_uid);
+		//printf("%ld\n", list->sb->st_nlink);//number of links
+		//}
 		//printf("%o\n", list->sb->st_mode);
 		//printf("%d\t", de->d_type);
 		// 	  if(de->d_type == 4 && de->d_name != "." && de->d_name != "..")
-		//   {
-		// 		  printf("   ||   ");
-		// 		recu = opendir(de->d_name);
-		// 	 	 while((rec = readdir(recu)) != NULL)
-		// 	 	 {
+		//		{
+		// 			printf("   ||   ");
+		// 			recu = opendir(de->d_name);
+		//			while((rec = readdir(recu)) != NULL)
+		// 	 	{
 		// 	 		printf("%s\t", rec->d_name);
 		// 	 		stat(rec->d_name, &c);
 		// 	 	 }
 		// 		  printf("   ||   ");
 		// 	 }
 	}
-	/* code */
+	ft_listsort(list);
 	// printf(" file position within stream = 		%d\n", de->d_reclen);
 	// printf("		           File Type = 		%d\n", de->d_type);
 	// printf("		           stat mode = 		%o\n", sb.st_mode);//prints mode in octal
