1b9adeb6f439b0bffa10b3256d5bbe840fa25c1a
Zhain HELM <zhelm@c5r5s5.wethinkcode.co.za>
2019-07-29 15:21:45 +0200

    OK

--- main.c
+++ main.c
@@ -14,6 +14,77 @@
 #include "libft/libft.h"
 #include <stdlib.h>
 
+void ft_assign_dir(t_ls **head, char *dir)
+{
+	char *dirtmp1;
+	t_ls *ptr;
+
+	dirtmp1 = ft_strjoin(dir, "/"); //should be another function
+	ptr = *head;
+	while (ptr)
+	{
+		if (ptr->next != NULL)
+			ptr = ptr->next;
+		else
+			break;
+	}
+	ptr->directory = ft_strjoin(dirtmp1, ptr->de->d_name);
+}
+
+void ft_ls_mode(struct stat sb)
+{
+	size_t i;
+
+	i = -1;
+	while (++i <= 8)
+	{
+		if ((sb.st_mode & S_IRUSR) && i == 0)
+			ft_putchar('r');
+		else if( (sb.st_mode & S_IWUSR) && i == 1)
+			ft_putchar('w');
+		else if( (sb.st_mode & S_IXUSR) && i == 2)
+			ft_putchar('x');
+		else if( (sb.st_mode & S_IRGRP) && i == 3)
+			ft_putchar('r');
+		else if( (sb.st_mode & S_IWGRP) && i == 4)
+			ft_putchar('w');
+		else if( (sb.st_mode & S_IXGRP) && i == 5)
+			ft_putchar('x');
+		else if( (sb.st_mode & S_IROTH) && i == 6)
+			ft_putchar('r');
+		else if( (sb.st_mode & S_IWOTH) && i == 7)
+			ft_putchar('w');
+		else if( (sb.st_mode & S_IXOTH) && i == 8)
+			ft_putchar('x');
+		else
+			ft_putchar('-');
+	}
+}
+
+void ft_ls_l(t_ls **head)
+{
+	t_ls *tmp;
+	t_ls *ptr;
+	struct stat sb;
+
+	tmp = *head;
+	while (tmp != NULL)
+	{
+		//printf("%s\t", tmp->de->d_name);//not sorted yet
+
+		stat(tmp->directory, &sb);
+		ft_ls_mode(sb);
+		printf("\n");
+		ptr = tmp;
+		// if(tmp->next == NULL)
+		// 	break;
+		tmp = tmp->next;
+		//free(ptr);
+	}
+
+	//rwx, block size, ||links||, ||user name||, ||group name||, ||size||, day, month, time, ||name||
+}
+
 void ft_listsort(t_ls **head)
 {
 	t_ls *tmp;
@@ -21,15 +92,18 @@ void ft_listsort(t_ls **head)
 	t_ls *tmp1;
 	t_ls *ptr;
 	size_t i;
+	char *tp;
 
-	i = 0;
 	ptr = *head;
 	while (ptr->next != NULL)
 	{
 		if (ft_strcmp(ptr->de->d_name, ptr->next->de->d_name) > 0)
 		{
+			tp = ptr->directory;
 			de = ptr->de;
+			ptr->directory = ptr->next->directory;
 			ptr->de = ptr->next->de;
+			ptr->next->directory = tp;
 			ptr->next->de = de;
 			ptr = *head;
 		}
@@ -47,20 +121,12 @@ void ft_ls_seg_lstadd(t_ls **head, t_ls **seg)
 	ptr = *head;
 	tmp = (*head)->next;
 	(*head)->next = *seg;
-	while(ptr->next != NULL)
+	while (ptr->next != NULL)
 		ptr = ptr->next;
 	ptr->next = tmp;
 	ptr = *head;
 	(*head) = (*head)->next;
 	free(ptr);
-	// *head = ptr->next;
-	while((*head))
-	// {
-	// 	printf("\t%s\t", (*head)->de->d_name);
-			*head = (*head)->next;//This prevents the segfault because it moves the pointer of head one up
-	// }
-	//set head->next = segment and iterate through. set the end of segment equal to head->next if it dit exist else set it equal to null
-	//free the head and set head->next = head
 }
 
 t_ls *ft_listrec(DIR *dr, t_ls **head, char *dir)
@@ -75,48 +141,53 @@ t_ls *ft_listrec(DIR *dr, t_ls **head, char *dir)
 	segment = NULL;
 	struct dirent *de;
 	tmp = NULL;
-	if(*head == NULL)
+	if (*head == NULL)
 		dir = ft_strdup(".");
 	else
 		dir = (*head)->directory;
 	dr = opendir(dir);
+	printf("%s:\n", dir);
 	while ((de = readdir(dr)) != NULL)
 	{
+
 		if (tmp == NULL)
 			tmp = ft_ls_lstnew(de, NULL);
 		else
 			ft_ls_lstadd(&tmp, ft_ls_lstnew(de, NULL));
+		ft_assign_dir(&tmp, dir);
 		if (de->d_type == 4 && strcmp(de->d_name, ".") != 0 && ft_strcmp(de->d_name, "..") != 0)
 		{
 			if (segment == NULL)
-				segment = ft_ls_lstnew(de, dir);
+				segment = ft_ls_lstnew(de, NULL);
 			else
-				ft_ls_lstadd(&segment, ft_ls_lstnew(de, dir));
+				ft_ls_lstadd(&segment, ft_ls_lstnew(de, NULL));
+			ft_assign_dir(&segment, dir);
 		}
 	}
-
-	if (tmp->next != NULL)//display tmp here before free
+	if (tmp->next != NULL) //display tmp here before free
 		ft_listsort(&tmp);
-	if (segment->next != NULL)
-		ft_listsort(&segment);//was close to doing this part again. thought I was sorting thead head; luckily it was only segments
+	ptr = tmp;
+	// while(ptr != NULL)
+	// {
+	// 	printf("%s\t", ptr->de->d_name);
+	ft_ls_l(&tmp);
+	// ptr = ptr->next;
+	// }
+	printf("\n\n");
+
+	if (segment != NULL)
+		ft_listsort(&segment); //was close to doing this part again. thought I was sorting the head; luckily it was only segments
+	ptr = segment;
+
 	if (*head == NULL && segment != NULL)
-		*head = segment;//it needs an else statement here because this is only first case
-		// assignment of directory works. just need to find a way to make it recurse
-	else if((*head)->next != NULL)//sunting here
+		*head = segment;
+	else if ((*head) != NULL) //sunting here
 		ft_ls_seg_lstadd(head, &segment);
-	//if((*head)->next != NULL)
 	closedir(dr);
-	if(*head != NULL && (*head)->next != NULL)//not a good way of doing the directory. use segments instead
-	{
-		dirtmp = (*head)->directory;
-		dirtmp1 = ft_strjoin((*head)->directory, "/");
-		(*head)->directory = ft_strjoin(dirtmp1, (*head)->de->d_name);
-	}
-	// 	ft_listrec(dr, head, dir);
-	if((*head) != NULL)//close the dir maybe
-	 	ft_listrec(dr, head, (*head)->directory);//it segfaults because of the function seg_lstadd;
-	// else
-		return 0;
+	if ((*head) != NULL)						  //close the dir maybe
+		ft_listrec(dr, head, (*head)->directory); //it segfaults because of the function seg_lstadd;
+												  // else
+	return 0;
 	// return *head;
 }
 
@@ -157,14 +228,16 @@ int main() //main to test the ctime and mtime
 	// 	//printf("%ld\n", list->sb->st_nlink);//number of links
 	// 	//}
 	// 	//printf("%o\n", list->sb->st_mode);
- 	//	//printf("%d\t", de->d_type);
+	//	//printf("%d\t", de->d_type);
 	// 	//       }
 	// }
 	closedir(dr);
 	dr = opendir(".");
 
 	i = 0;
-	ft_listrec(dr, &head, dir);
+	ft_listrec(dr, &head, dir); ///////////////////////////////////////////////////////////////////////
+
+	// ft_ls_l(dir);
 	// ft_recurselen(list);
 
 	//line = ft_memalloc(12);
@@ -180,8 +253,7 @@ int main() //main to test the ctime and mtime
 	// printf("                       group name =          %s\n", gr->gr_name);
 	// printf("   time of last modification =               %s\n\n", ctime(&sb.st_ctime));
 	//      printf("name = %s\n", de->d_name);
-
-	//printf( (sb.st_mode & S_IRUSR) ? "r\n" : "-\n");
 }
 //file mode, links(are these hard links or soft links or both?), owner name, group name, number of bytes ,  month, day file was last modified, time file was last modified, and the pathname
 //              ||-rw-r--r--||   1//just figure out how to get this    ||zhelm||   ||student||     ||20430||           ||Jul||              ||17||                        ||12:08||                     ||Notes||                  ||Notes||
+//
